68f76d0cc2a2ee53f6c60f78ae25d597e8a42e5c
rouanw <rouan@carbontechlab.com>
2021-02-11 11:03:43 +0200

    more forgiving database stub

--- database.js
+++ database.js
@@ -5,7 +5,7 @@ module.exports = {
   create() {
     return new Promise((resolve) => resolve());
   },
-  update(username) {
-    return new Promise((resolve) => resolve({ firstName: 'Ada', lastName: 'Lovelace' }));
+  update(username, profile) {
+    return new Promise((resolve) => resolve(profile));
   },
 };
